a63cf85431771b223df4100c84299ce435831482
Mathys Cogne--Foucault <mcogne--@paul-f4Br5s3.clusters.42paris.fr>
2025-01-04 17:44:08 +0100

    Fix Signal in Shild process

--- src/exec/signal/signal.c
+++ src/exec/signal/signal.c
@@ -6,44 +6,32 @@
 /*   By: mcogne-- <mcogne--@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 20:05:06 by mcogne--          #+#    #+#             */
-/*   Updated: 2025/01/03 23:25:24 by mcogne--         ###   ########.fr       */
+/*   Updated: 2025/01/04 16:29:09 by mcogne--         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-// • Handle ctrl-C, ctrl-D and ctrl-\ which should behave like in bash.
-// • In interactive mode:
-// ◦ ctrl-C displays a new prompt on a new line.
-// ◦ ctrl-D exits the shell.
-// ◦ ctrl-\ does nothing.
-/*
-** CTRL-D
-** Manage in get_input.c, because CTRL-D = EOF, so input is NULL and exit shell
-**
-*/
-/*
-** CTRL-\
-** Nothing
-** Interactive mode: TODO
-*/
-// void	handler_sigquit(int sig)
-// {
-// 	(void)sig;
-// }
-
 /*
 ** CTRL-C
 ** Clear line and Prompt a new line
-** Interactive mode: TODO
+** Interactive mode: Just prompt new line
 */
-void	handler_sigint(int sig)
+void	handler_sigint(int sig, siginfo_t *info, void *context)
 {
 	(void)sig;
-	rl_replace_line("", 0);
-	rl_on_new_line();
-	ft_printf("\n");
-	rl_redisplay();
+	(void)context;
+	if (info->si_pid == getpid())
+	{
+		rl_replace_line("", 0);
+		rl_on_new_line();
+		ft_printf("\n");
+		rl_redisplay();
+	}
+	else
+	{
+		ft_printf("\n");
+	}
 }
 
 void	setup_signal(void)
@@ -51,8 +39,8 @@ void	setup_signal(void)
 	struct sigaction	sa;
 
 	sigemptyset(&sa.sa_mask);
-	sa.sa_flags = SA_RESTART;
-	sa.sa_handler = handler_sigint;
+	sa.sa_sigaction = &handler_sigint;
+	sa.sa_flags = SA_SIGINFO;
 	sigaction(SIGINT, &sa, NULL);
 	sa.sa_handler = SIG_IGN;
 	sigaction(SIGQUIT, &sa, NULL);
